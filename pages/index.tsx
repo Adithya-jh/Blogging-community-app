013fcb7f2920589f1e8c520434643e64d73f430f
Adithya <jhadith88@gmail.com>
2022-05-20 19:07:41 +0530

    updated head

--- pages/index.tsx
+++ pages/index.tsx
@@ -16,7 +16,7 @@ export default function Home ({posts} : Props) {
   return (
     <div className="max-w-7xl mx-auto">
       <Head>
-        <title>Medium Blog</title>
+        <title>Ideate</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
